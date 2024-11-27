a4385c4ead43266bbfcef45e1265b66784fe3564
Zara Samm <zarasamm1996@gmail.com>
2024-11-27 15:04:51 +0000

    update views for delete functionality

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-zasamm-runwild-iyf4ti37hrv.ws.codeinstitute-ide.net', '.herokuapp.com']
 
@@ -52,8 +52,7 @@ INSTALLED_APPS = [
     'crispy_bootstrap5',
     'cloudinary_storage',
     'cloudinary',
-    'run_wild',
-
+    'run_wild',    
     
 ]
 
--- run_wild/urls.py
+++ run_wild/urls.py
@@ -2,13 +2,14 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.HomePage.as_view(), name='home'),
-    path('quests/', views.QuestList.as_view(), name='quests'),
-    path('quest/<slug:slug>/', views.quest_post, name='quest_post'),
+     path('', views.HomePage.as_view(), name='home'),
+     path('quest-records/', views.QuestRecordList.as_view(), name='quest_records'),
+     path('quests/', views.QuestList.as_view(), name='quests'),
+     path('quest/<slug:slug>/', views.quest_post, name='quest_post'),
      path('quest/<slug:slug>/edit/<int:quest_record_id>/', 
          views.record_edit, name='record_edit'),
-    path('get-run-data/<int:quest_record_id>/', 
+     path('get-run-data/<int:quest_record_id>/', 
          views.get_run_data, name='get_run_data'),
-    path('quest/<slug:slug>/delete/<int:quest_record_id>/',
+     path('quest/<slug:slug>/delete/<int:quest_record_id>/',
          views.record_delete, name='record_delete'),
 ]
