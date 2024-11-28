e81ced90f43f9e5d26ba559b7f1f9f67d25a06d9
Zara Samm <zarasamm1996@gmail.com>
2024-11-28 13:08:54 +0000

    add: timedelta - form class for user to select hours, mins and seconds

--- run_wild/forms.py
+++ run_wild/forms.py
@@ -1,11 +1,32 @@
 from django import forms
+from datetime import timedelta
 from .models import QuestRecord
 
 class QuestCompletionForm(forms.ModelForm):
-    completion_time = forms.FloatField(
-        label = 'Completion Time (Mins)'
-    )
-    
+    hours = forms.IntegerField(min_value=0)
+    minutes = forms.IntegerField(min_value=0, max_value=59)
+    seconds = forms.IntegerField(min_value=0, max_value=59)
+
     class Meta:
         model = QuestRecord
-        fields = ('completion_time',)
+        fields = ['hours', 'minutes', 'seconds']
+
+    def clean(self):
+        cleaned_data = super().clean()
+        hours = cleaned_data.get('hours', 0)
+        minutes = cleaned_data.get('minutes', 0)
+        seconds = cleaned_data.get('seconds', 0)
+        
+        cleaned_data['completion_time'] = timedelta(
+            hours=hours,
+            minutes=minutes,
+            seconds=seconds
+        )
+        return cleaned_data
+
+    def save(self, commit=True):
+        instance = super().save(commit=False)
+        instance.completion_time = self.cleaned_data['completion_time']
+        if commit:
+            instance.save()
+        return instance
