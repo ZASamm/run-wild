745e4487ea8b6fceb99e0668354ffeba380e187c
Zara Samm <zarasamm1996@gmail.com>
2024-11-29 09:53:12 +0000

    Add: Placeholder text for instructions on what to add to the form

--- about_page/forms.py
+++ about_page/forms.py
@@ -5,3 +5,6 @@ class NewQuestForm(forms.ModelForm):
     class Meta:
         model = RequestNewQuest
         fields = ('fname', 'lname', 'email', 'message', 'image')
+        widgets = {
+            'message': forms.Textarea(attrs={'placeholder': 'Please provide the Name, short description, distance, elevation gain and max elevation.'}),
+        }
