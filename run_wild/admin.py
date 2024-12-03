d6ae12a15ed81910c6909d5defab9602cd212030
Zara Samm <zarasamm1996@gmail.com>
2024-12-03 17:03:47 +0000

    Add: admin interface view for QuestPost and QuestRecord for better updating

--- run_wild/admin.py
+++ run_wild/admin.py
@@ -1,15 +1,35 @@
 from django.contrib import admin
-from .models import QuestPost, QuestRecord
+from django.utils.html import format_html
 from django_summernote.admin import SummernoteModelAdmin
+from .models import QuestPost, QuestRecord
 
 @admin.register(QuestPost)
-class PostAdmin(SummernoteModelAdmin):
-
-    list_display = ('title', 'slug')
+class QuestPostAdmin(SummernoteModelAdmin):
+    list_display = ('title', 'difficulty', 'distance', 'preview_map')
     search_fields = ['title']
     prepopulated_fields = {'slug': ('title',)}
-    summernote_fields = ('content',)
+    summernote_fields = ('description',)
+    
+    def preview_map(self, obj):
+        if obj.map_route and obj.map_route.url:
+            return format_html('<img src="{}" style="max-height: 50px;"/>', obj.map_route.url)
+        return "No map"
+    preview_map.short_description = 'Route Map'
+
+@admin.register(QuestRecord)
+class QuestRecordAdmin(admin.ModelAdmin):
+    list_display = ('quest', 'runner', 'completion_time', 'tokens_earned', 'approved')
+    list_filter = ('approved', 'is_personal_best')
+    search_fields = ['runner__username', 'quest__title']
+    readonly_fields = ('tokens_earned', 'pace', 'is_personal_best')
+    actions = ['approve_records', 'unapprove_records']
+    
+    def approve_records(self, request, queryset):
+        queryset.update(approved=True)
+    approve_records.short_description = "Approve selected records"
+    
+    def unapprove_records(self, request, queryset):
+        queryset.update(approved=False)
+    unapprove_records.short_description = "Unapprove selected records"
 
 # Register your models here.
-
-admin.site.register(QuestRecord)
