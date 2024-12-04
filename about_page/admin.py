e9695a0140fa59a0a1288eedcc7482dd814410ca
Zara Samm <zarasamm1996@gmail.com>
2024-12-04 13:14:02 +0000

    Refactor: Python linter fixes

--- about_page/admin.py
+++ about_page/admin.py
@@ -3,31 +3,41 @@ from django_summernote.admin import SummernoteModelAdmin
 from django.utils.html import format_html
 from .models import About, RequestNewQuest
 
+
 @admin.register(About)
 class AboutAdmin(SummernoteModelAdmin):
     list_display = ('title', 'preview_pic', 'updated_on')
     summernote_fields = ('content',)
-    
+
     def preview_pic(self, obj):
         if obj.profile_pic and obj.profile_pic.url:
-            return format_html('<img src="{}" style="max-height: 50px;"/>', obj.profile_pic.url)
+            return format_html(
+                '<img src="{}" style="max-height: 50px;"/>',
+                obj.profile_pic.url
+                )
+
         return "No image"
+
     preview_pic.short_description = 'Profile Picture'
 
-@admin.register(RequestNewQuest) 
+
+@admin.register(RequestNewQuest)
 class RequestNewQuestAdmin(admin.ModelAdmin):
     list_display = ('full_name', 'email', 'preview_image', 'read')
     list_filter = ('read',)
     search_fields = ['fname', 'lname', 'email', 'message']
     actions = ['mark_as_read', 'mark_as_unread']
-    
+
     def full_name(self, obj):
         return f"{obj.fname} {obj.lname}"
+
     full_name.short_description = 'Name'
-    
+
     def preview_image(self, obj):
         if obj.image and obj.image.url:
-            return format_html('<img src="{}" style="max-height: 50px;"/>', obj.image.url)
+            return format_html(
+                '<img src="{}" style="max-height: 50px;"/>', obj.image.url
+                )
         return "No image"
     preview_image.short_description = 'Image'
 
