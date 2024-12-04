fb638c9c1c2eec9acf11a0161c1e49a1dd517c18
Zara Samm <zarasamm1996@gmail.com>
2024-12-04 14:53:58 +0000

    Add: doc strings

--- about_page/models.py
+++ about_page/models.py
@@ -5,6 +5,15 @@ from cloudinary.models import CloudinaryField
 
 
 class About(models.Model):
+    """
+    Model representing the About page content.
+
+    Attributes:
+        title (str): Title of the about page section.
+        profile_pic (CloudinaryField): Profile picture for the about page.
+        updated_on (datetime): Timestamp of the last update.
+        content (str): Content of the about page section.
+    """
     title = models.CharField(max_length=200)
     profile_pic = CloudinaryField('image', default='placeholder')
     updated_on = models.DateTimeField(auto_now=True)
@@ -15,6 +24,19 @@ class About(models.Model):
 
 
 class RequestNewQuest(models.Model):
+
+    """
+    Model representing a request for a new quest.
+
+    Attributes:
+        fname (str): First name of the requester.
+        lname (str): Last name of the requester.
+        email (str): Email address of the requester.
+        message (str): Message from the requester.
+        image (CloudinaryField): Image provided by the requester.
+        read (bool): Status indicating if the request has been read.
+    """
+
     fname = models.CharField('First Name', max_length=200)
     lname = models.CharField('Last Name', max_length=200)
     email = models.EmailField()
