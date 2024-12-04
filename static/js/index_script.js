341cf4d670622e8ff63130a1e38aec7524350fad
Zara Samm <zarasamm1996@gmail.com>
2024-12-04 14:53:48 +0000

    refactor: add domcontentloaded. update let variables to const

--- static/js/index_script.js
+++ static/js/index_script.js
@@ -1,15 +1,32 @@
-let valueDisplays = document.querySelectorAll(".num");
-let interval = 4000;
+/**
+ * Credit to https://dev.to/codingcss/responsive-number-counting-animation-with-javascript-project-with-source-code-2m5
+ * 
+ * Initializes the number count-up animation for elements with the class "num".
+ * The count-up animation starts from 0 and ends at the value specified in the "data-val" attribute.
+ * The duration of the animation is distributed evenly based on the interval and the end value.
+ */
 
-valueDisplays.forEach((valueDisplay) => {
-  let startValue = 0;
-  let endValue = parseInt(valueDisplay.getAttribute("data-val"));
-  let duration = Math.floor(interval / endValue);
-  let counter = setInterval(function () {
-    startValue += 1;
-    valueDisplay.textContent = startValue;
-    if (startValue == endValue) {
-      clearInterval(counter);
-    }
-  }, duration);
+document.addEventListener('DOMContentLoaded', () => {
+  const valueDisplays = document.querySelectorAll(".num");
+  const interval = 4000;
+
+  valueDisplays.forEach(valueDisplay => {
+      let startValue = 0;
+      const endValue = parseInt(valueDisplay.getAttribute("data-val"), 10);
+
+      if (isNaN(endValue)) {
+          console.error("Invalid data-val attribute:", valueDisplay.getAttribute("data-val"));
+          return;
+      }
+
+      const duration = Math.floor(interval / endValue);
+
+      const counter = setInterval(() => {
+          startValue += 1;
+          valueDisplay.textContent = startValue;
+          if (startValue === endValue) {
+              clearInterval(counter);
+          }
+      }, duration);
+  });
 });
