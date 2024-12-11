001cac2752a72d452bac2bfc76a0d8636ff2de20
Zara Samm <zarasamm1996@gmail.com>
2024-12-11 13:24:40 +0000

    Add: About page admin view

--- about_page/admin.py
+++ about_page/admin.py
@@ -1,9 +1,25 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 from django.utils.html import format_html
-from .models import RequestNewQuest
+from .models import About, RequestNewQuest
 
 
 # Register your models here.
+@admin.register(About)
+class AboutAdmin(SummernoteModelAdmin):
+    list_display = ('title', 'preview_pic', 'updated_on')
+    summernote_fields = ('content',)
+
+    def preview_pic(self, obj):
+        if obj.profile_pic and obj.profile_pic.url:
+            return format_html(
+                '<img src="{}" style="max-height: 50px;"/>',
+                obj.profile_pic.url
+                )
+        return "No image"
+    preview_pic.short_description = 'Profile Picture'
+
+
 @admin.register(RequestNewQuest)
 class RequestNewQuestAdmin(admin.ModelAdmin):
     list_display = ('full_name', 'email', 'preview_image', 'read')
